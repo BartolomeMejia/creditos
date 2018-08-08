54a553e9f60b39cfaf0317efd7f92338387153ed
Erick Pac <erick.pac@fox.com>
2018-08-07 19:13:22 -0600

    alidate if the quantity to pay is bigger than zero and is not empty

--- public/src/scripts/lazyload/controllers/ClientesController.js
+++ public/src/scripts/lazyload/controllers/ClientesController.js
@@ -1,202 +1,187 @@
-;(function() 
-{
-	"use strict";
+; (function () {
+  "use strict";
 
-	angular.module("app.clientes", ["app.constants"])
+  angular.module("app.clientes", ["app.constants"])
 
-	.controller("ClientesController", ["$scope", "$filter", "$http", "$modal", "$interval", "API_URL", function($scope, $filter, $http, $modal, $timeout, API_URL)  {	
-		
-		//Variables generales
-		$scope.datas = [];
-		$scope.currentPageStores = [];
-		$scope.searchKeywords = "";
-		$scope.filteredData = [];	
-		$scope.row = "";
-		$scope.numPerPageOpts = [5, 10, 25, 50, 100];
-		$scope.numPerPage = $scope.numPerPageOpts[1];
-		$scope.currentPage = 1;
-		$scope.positionModel = "topRight";
-		$scope.toasts = [];
-		var modal;
+    .controller("ClientesController", ["$scope", "$filter", "$http", "$modal", "$interval", "API_URL", function ($scope, $filter, $http, $modal, $timeout, API_URL) {
 
-		$scope.LlenarTabla = function()
-		{
-			$http({
-				method: 'GET',
-			  	url: 	API_URL+'clientes'
-			})
-			.then(function successCallback(response)  {
-			    $scope.datas = response.data.records;
-				$scope.search();
-				$scope.select($scope.currentPage);
-			}, 
-			function errorCallback(response)  {			
-			   console.log( response.data.message );
-			});
-		}
+      //Variables generales
+      $scope.datas = [];
+      $scope.currentPageStores = [];
+      $scope.searchKeywords = "";
+      $scope.filteredData = [];
+      $scope.row = "";
+      $scope.numPerPageOpts = [5, 10, 25, 50, 100];
+      $scope.numPerPage = $scope.numPerPageOpts[1];
+      $scope.currentPage = 1;
+      $scope.positionModel = "topRight";
+      $scope.toasts = [];
+      var modal;
 
-		// FUNCIONES DE DATATABLE
-		$scope.select = function(page) {
-			var start = (page - 1)*$scope.numPerPage,
-				end = start + $scope.numPerPage;
+      $scope.LlenarTabla = () => {
+        $http({
+          method: 'GET',
+          url: API_URL + 'clientes'
+        }).then(function successCallback(response) {
+          $scope.datas = response.data.records;
+          $scope.search();
+          $scope.select($scope.currentPage);
+        }, function errorCallback(response) {
+          console.log(response.data.message);
+        });
+      }
 
-			$scope.currentPageStores = $scope.filteredData.slice(start, end);
-		}
+      // FUNCIONES DE DATATABLE
+      $scope.select = function (page) {
+        var start = (page - 1) * $scope.numPerPage,
+          end = start + $scope.numPerPage;
 
-		$scope.onFilterChange = function() {
-			$scope.select(1);
-			$scope.currentPage = 1;
-			$scope.row = '';
-		}
+        $scope.currentPageStores = $scope.filteredData.slice(start, end);
+      }
 
-		$scope.onNumPerPageChange = function() {
-			$scope.select(1);
-			$scope.currentPage = 1;
-		}
+      $scope.onFilterChange = function () {
+        $scope.select(1);
+        $scope.currentPage = 1;
+        $scope.row = '';
+      }
 
-		$scope.onOrderChange = function() {
-			$scope.select(1);
-			$scope.currentPage = 1;
-		}
+      $scope.onNumPerPageChange = function () {
+        $scope.select(1);
+        $scope.currentPage = 1;
+      }
 
-		$scope.search = function() {
-			$scope.filteredData = $filter("filter")($scope.datas, $scope.searchKeywords);
-			$scope.onFilterChange();		
-		}
+      $scope.onOrderChange = function () {
+        $scope.select(1);
+        $scope.currentPage = 1;
+      }
 
-		$scope.order = function(rowName) {
-			if($scope.row == rowName)
-				return;
-			$scope.row = rowName;
-			$scope.filteredData = $filter('orderBy')($scope.datas, rowName);
-			$scope.onOrderChange();
-		}	
+      $scope.search = function () {
+        $scope.filteredData = $filter("filter")($scope.datas, $scope.searchKeywords);
+        $scope.onFilterChange();
+      }
 
-		$scope.LlenarTabla();
+      $scope.order = function (rowName) {
+        if ($scope.row == rowName)
+          return;
+        $scope.row = rowName;
+        $scope.filteredData = $filter('orderBy')($scope.datas, rowName);
+        $scope.onOrderChange();
+      }
 
-		// Función para Toast
-		$scope.createToast = function(tipo, mensaje) {
-			$scope.toasts.push({
-				anim: "bouncyflip",
-				type: tipo,
-				msg: mensaje
-			});
-		}
+      $scope.LlenarTabla();
 
-		$scope.closeAlert = function(index) {
-			$scope.toasts.splice(index, 1);
-		}
+      // Función para Toast
+      $scope.createToast = function (tipo, mensaje) {
+        $scope.toasts.push({
+          anim: "bouncyflip",
+          type: tipo,
+          msg: mensaje
+        });
+      }
 
-		$scope.saveData = function( cliente ) {
-			if ($scope.accion == 'crear') {
-				$http({
-					method: 'POST',
-				  	url: 	API_URL+'clientes',
-				  	data: 	cliente
-				})
-				.then(function successCallback(response) {
-					if( response.data.result ) {
-					    $scope.LlenarTabla();
-					    modal.close();
-					    $scope.createToast("success", "<strong>Éxito: </strong>"+response.data.message);
-					    $timeout( function(){ $scope.closeAlert(0); }, 5000);
-					}
-					else {
-						$scope.createToast("danger", "<strong>Error: </strong>"+response.data.message);
-					    $timeout( function(){ $scope.closeAlert(0); }, 5000);	
-					}
-				}, 
-				function errorCallback(response) {
-				   console.log( response.data.message );
-				});
-			}
-			else if ($scope.accion == 'editar') {
-				$http({
-					method: 'PUT',
-				  	url: 	API_URL+'clientes/'+cliente.id,
-				  	data: 	cliente
-				})
-				.then(function successCallback(response) {
-					if( response.data.result ) {
-					    $scope.LlenarTabla();
-					    modal.close();
-					    $scope.createToast("success", "<strong>Éxito: </strong>"+response.data.message);
-					    $timeout( function(){ $scope.closeAlert(0); }, 3000);
-					}
-					else {
-						$scope.createToast("danger", "<strong>Error: </strong>"+response.data.message);
-					    $timeout( function(){ $scope.closeAlert(0); }, 5000);	
-					}
-				}, 
-				function errorCallback(response) {
-				   console.log( response.data.message );
-				});
-			}
-			else if ($scope.accion == 'eliminar') {
-				$http({
-					method: 'DELETE',
-				  	url: 	API_URL+'clientes/'+cliente.id,
-				})
-				.then(function successCallback(response) {
-					if( response.data.result ) {
-					    $scope.LlenarTabla();
-					    modal.close();
-					    $scope.createToast("success", "<strong>Éxito: </strong>"+response.data.message);
-					    $timeout( function(){ $scope.closeAlert(0); }, 3000);
-					}
-					else {
-						$scope.createToast("danger", "<strong>Error: </strong>"+response.data.message);
-					    $timeout( function(){ $scope.closeAlert(0); }, 5000);	
-					}
-				}, 
-				function errorCallback(response) {
-				   console.log( response.data.message );
-				});
-			}
-		}
+      $scope.closeAlert = function (index) {
+        $scope.toasts.splice(index, 1);
+      }
 
-		// Funciones para Modales
-		$scope.modalCreateOpen = function() {
-			$scope.cliente = {};
-			$scope.accion = 'crear';
+      $scope.saveData = function (cliente) {
+        if ($scope.accion == 'crear') {
+          $http({
+            method: 'POST',
+            url: API_URL + 'clientes',
+            data: cliente
+          }).then(function successCallback(response) {
+            if (response.data.result) {
+              $scope.LlenarTabla();
+              modal.close();
+              $scope.createToast("success", "<strong>Éxito: </strong>" + response.data.message);
+              $timeout(function () { $scope.closeAlert(0); }, 5000);
+            } else {
+              $scope.createToast("danger", "<strong>Error: </strong>" + response.data.message);
+              $timeout(function () { $scope.closeAlert(0); }, 5000);
+            }
+          }, function errorCallback(response) {
+            console.log(response.data.message);
+          });
+        } else if ($scope.accion == 'editar') {
+          $http({
+            method: 'PUT',
+            url: API_URL + 'clientes/' + cliente.id,
+            data: cliente
+          }).then(function successCallback(response) {
+            if (response.data.result) {
+              $scope.LlenarTabla();
+              modal.close();
+              $scope.createToast("success", "<strong>Éxito: </strong>" + response.data.message);
+              $timeout(function () { $scope.closeAlert(0); }, 3000);
+            } else {
+              $scope.createToast("danger", "<strong>Error: </strong>" + response.data.message);
+              $timeout(function () { $scope.closeAlert(0); }, 5000);
+            }
+          }, function errorCallback(response) {
+            console.log(response.data.message);
+          });
+        } else if ($scope.accion == 'eliminar') {
+          $http({
+            method: 'DELETE',
+            url: API_URL + 'clientes/' + cliente.id,
+          }).then(function successCallback(response) {
+            if (response.data.result) {
+              $scope.LlenarTabla();
+              modal.close();
+              $scope.createToast("success", "<strong>Éxito: </strong>" + response.data.message);
+              $timeout(function () { $scope.closeAlert(0); }, 3000);
+            } else {
+              $scope.createToast("danger", "<strong>Error: </strong>" + response.data.message);
+              $timeout(function () { $scope.closeAlert(0); }, 5000);
+            }
+          }, function errorCallback(response) {
+            console.log(response.data.message);
+          });
+        }
+      }
 
-			modal = $modal.open({
-				templateUrl: "views/clientes/modal.html",
-				scope: $scope,
-				size: "md",
-				resolve: function() {},
-				windowClass: "default"
-			});
-		}
+      // Funciones para Modales
+      $scope.modalCreateOpen = function () {
+        $scope.cliente = {};
+        $scope.accion = 'crear';
 
-		$scope.modalEditOpen = function(data) {			
-			$scope.accion = 'editar';
-			$scope.cliente = data;
+        modal = $modal.open({
+          templateUrl: "views/clientes/modal.html",
+          scope: $scope,
+          size: "md",
+          resolve: function () { },
+          windowClass: "default"
+        });
+      }
 
-			modal = $modal.open({
-				templateUrl: "views/clientes/modal.html",
-				scope: $scope,
-				size: "md",
-				resolve: function() {},
-				windowClass: "default"
-			});
-		}
+      $scope.modalEditOpen = function (data) {
+        $scope.accion = 'editar';
+        $scope.cliente = data;
 
-		$scope.modalDeleteOpen = function(data) {			
-			$scope.accion = 'eliminar';
+        modal = $modal.open({
+          templateUrl: "views/clientes/modal.html",
+          scope: $scope,
+          size: "md",
+          resolve: function () { },
+          windowClass: "default"
+        });
+      }
 
-			$scope.cliente = data;
-			modal = $modal.open({
-				templateUrl: "views/clientes/modal.html",
-				scope: $scope,
-				size: "md",
-				resolve: function() {},
-				windowClass: "default"
-			});
-		}
+      $scope.modalDeleteOpen = function (data) {
+        $scope.accion = 'eliminar';
 
-		$scope.modalClose = function() {
-			modal.close();
-		}
-	}])
+        $scope.cliente = data;
+        modal = $modal.open({
+          templateUrl: "views/clientes/modal.html",
+          scope: $scope,
+          size: "md",
+          resolve: function () { },
+          windowClass: "default"
+        });
+      }
+
+      $scope.modalClose = function () {
+        modal.close();
+      }
+    }])
 }())
