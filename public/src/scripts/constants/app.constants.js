996ce769a7c306ec377c23b2991098ed442065ae
Bartolomé Mejía <bartolome.mejia@fox.com>
2019-04-08 15:27:04 -0600

    generate build fix list customers

--- public/src/scripts/constants/app.constants.js
+++ public/src/scripts/constants/app.constants.js
@@ -3,6 +3,6 @@
 	angular.module("app.constants", [])
 
 	//.constant('API_URL', 'http://localhost:8080/creditos/public/ws/')
-	.constant('API_URL', 'http://206.189.68.122/ws/') //Mario
-	//.constant('API_URL', 'http://157.230.9.234/ws/') //Pedro
+	//.constant('API_URL', 'http://206.189.68.122/ws/') //Mario
+	.constant('API_URL', 'http://157.230.9.234/ws/') //Pedro
 }())
