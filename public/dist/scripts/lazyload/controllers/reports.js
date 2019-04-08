97755cea9fbce54e0f9d7e9f403c0f139855a992
Bartolomé Mejía <bartolome.mejia@fox.com>
2019-04-08 13:48:17 -0600

    generate build for 157

--- public/dist/scripts/lazyload/controllers/reports.js
+++ public/dist/scripts/lazyload/controllers/reports.js
@@ -1,53 +1,85 @@
 ; (function () {
     "use strict";
   
-    angular.module("app.reports", ["app.constants", 'app.service.collector', 'app.service.plan', 'app.service.report', 'app.service.pdfs'])
+    angular.module("app.reports", ["app.constants", 'app.service.collector', 'app.service.plan', 'app.service.report', 'app.service.pdfs', 'app.service.branch'])
   
-      .controller("ReportsController", ["$scope", "$filter", "$http", "$modal", "$interval", 'collectorService', 'planService', 'reportService', 'pdfsService', 'API_URL', function ($scope, $filter, $http, $modal, $timeout, collectorService, planService, reportService, pdfsService, API_URL) {
+      .controller("ReportsController", ["$scope", "$filter", "$http", "$modal", "$interval", 'collectorService', 'planService', 'reportService', 'pdfsService', 'branchService', 'API_URL', function ($scope, $filter, $http, $modal, $timeout, collectorService, planService, reportService, pdfsService, branchService ,API_URL) {
 
-        $scope.positionModel = "topRight";
+        $scope.positionModel = "topRight"
         $scope.toasts = [];
         $scope.collectors = []
         $scope.plans = []
+        $scope.branchs = []
         $scope.customer = {}
-        var typeView = "";
-
-        loadCollectors()
-        loadPlanes()    
+        var branchSelected
+        var typeView = ""
         
-        function loadCollectors(){
+        initLoadInfo();
+
+        function initLoadInfo() {
+            if ($scope.usuario.tipo_usuarios_id == 1) {
+                loadBranches()
+            } else {
+                loadCollectors($scope.usuario.sucursales_id)
+                loadPlanes($scope.usuario.sucursales_id)
+            }
+        }
+
+        function loadCollectors(branch){
+            $scope.collectors = []
             collectorService.index().then(function (response) {
                 response.data.records.forEach(function (item) {
-                    if (item.sucursales_id == $scope.usuario.sucursales_id) {
+                    if (item.sucursales_id == branch) {
                         $scope.collectors.push(item)
                     }
                 })        
             })
         }
 
-        function loadPlanes(){
+        function loadPlanes(branch){
+            $scope.plans = []
             planService.plans().then(function (response) {
                 var itemAll = {}
                 itemAll.descripcion = "Todos";
                 itemAll.id = 0;
                 $scope.plans.push(itemAll)
                 response.data.records.forEach(function (item) {                    
-                    if (item.sucursales_id == $scope.usuario.sucursales_id) {
+                    if (item.sucursales_id == branch) {
                         $scope.plans.push(item)
                     }                                    
                 })        
             })
         }
 
+        function loadBranches() {
+            branchService.branchs().then(
+                function successCallback(response) {    
+                    
+                    if ($("#view").val() == "dates") {
+                        var itemAll = {}
+                        itemAll.descripcion = "Todos";
+                        itemAll.id = 0;
+                        $scope.branchs.push(itemAll)
+                    }
+
+                    response.data.records.forEach(function (item) {                                        
+                        $scope.branchs.push(item)                        
+                    })
+                },
+                function errorCallback(response) {
+                    console.log(response)						
+            });
+        }
+
         function generateReport(typeView){              
             var collector =  $scope.collector
             var dateInit = $("#date_init").val()
             var dateFinal = $("#date_fin").val()
-            var plan = $scope.plan == undefined ? "" : $scope.plan;
-            var branch = $scope.usuario.sucursales_id            
-
+            var plan = $scope.plan == undefined ? "" : $scope.plan;        
+            var branch = $scope.usuario.tipo_usuarios_id == 1 ? branchSelected : $scope.usuario.sucursales_id                        
             switch (typeView) {
-                case "dates" :                
+                case "dates" :     
+                    
                     if(dateInit != "" && dateInit != undefined && dateFinal != "" && dateFinal != undefined){
                         reportService.dates(dateInit, dateFinal, branch)
                             .then(function(response){
@@ -84,6 +116,21 @@
                     break
                 default:
                     break
+            }
+        }
+
+        $scope.branchSelected = function(branch){
+            if ($("#view").val() == "collectors") {
+                loadCollectors(branch)
+                loadPlanes(branch)    
+            } else {
+                if (branch == undefined) {
+                    $scope.createToast("danger", "<strong>Error: Debe seleccionar alguna de las opciones de sucursales. </strong>");
+                    $timeout(function () { $scope.closeAlert(0); }, 5000);
+                } else { 
+                    branchSelected = branch
+                }
+                
             }
         }
 
